b28efdd5fe60e48d8bccdf61a73545cf6f624625
pagcommerce <128087947+pagcommerce@users.noreply.github.com>
2023-03-29 17:08:42 -0300

    v 1.3.0 - Preço máximo do pedido e vencimento PIX

--- app/code/community/Pagcommerce/Payment/Model/Api/Pix.php
+++ app/code/community/Pagcommerce/Payment/Model/Api/Pix.php
@@ -3,6 +3,16 @@ class Pagcommerce_Payment_Model_Api_Pix extends Pagcommerce_Payment_Model_Api_Ab
 {
     public function getPixResponse(Mage_Sales_Model_Order $order){
         $data = $this->getBaseData($order);
+
+
+        $helper = Mage::helper('pagcommerce_payment');
+        $qtyDays = $helper->getConfig('pix', 'days');
+        $dueDate = strtotime(date('Y-m-d').' + '.$qtyDays.' days');
+        $dueDate = date('Y-m-d', $dueDate);
+
+        $data['due_date'] = $dueDate;
+        $data['additional_information'] = '';
+
         $response = $this->sendRequest('payment-pix', $data);
 
         if(isset($response['validation_messages'])){
